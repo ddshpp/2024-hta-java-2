1ba498e74edc947d9e9b8cbb6921f22fc11df017
ddshpp <dshp753@gmail.com>
2024-05-24 17:09:20 +0900

    docs: update README.md

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,10 +2,10 @@ import java.sql.SQLException;
 
 public class Application {
     public static void main(String[] args) throws SQLException {
-//        Statements.start();
-//        Statements.updateName("테스트입력", 213);
-//        Statements.getEmp();
-//        Statements.close();
+        Statements.start();
+        Statements.updateName("테스트입력", 213);
+        Statements.getEmp();
+        Statements.close();
         Statements.getTable();
     }
 }
