52d2ae4a40aab481236809f8b4832728fc514ec5
ddshpp <dshp753@gmail.com>
2024-05-24 14:12:04 +0900

    style: reformat code

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,10 +3,8 @@ import java.sql.SQLException;
 public class Application {
     public static void main(String[] args) throws SQLException {
         Statements.start();
-//        Statements.updateName("우히히히", 213);
+        Statements.updateName("우히히히", 213);
         Statements.getEmp();
         Statements.close();
-
-        System.out.println("하이하이");
     }
 }
