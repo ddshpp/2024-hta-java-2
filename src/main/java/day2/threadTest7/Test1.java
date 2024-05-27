5da937216ce8487ca9cb8b17e161819078667a36
ddshpp <dshp753@gmail.com>
2024-05-27 12:18:35 +0900

    test: interrupt test

---
+++ src/main/java/day2/threadTest7/Test1.java
@@ -0,0 +1,16 @@
+package day2.threadTest7;
+
+public class Test1 extends Thread {
+
+    @Override
+    public void run() {
+        int i = 10;
+
+        while (i != 0 && !isInterrupted()) {
+            System.out.println(i--);
+            for (long x = 0; x < 2500000000L; x++);
+        }
+
+        System.out.println("카운트 종료");
+    }
+}
