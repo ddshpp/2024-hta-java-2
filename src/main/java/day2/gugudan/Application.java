d1fbca729f46d5478dbb0ac66802fbede882fada
ddshpp <dshp753@gmail.com>
2024-05-27 11:39:29 +0900

    test: thread sleep test

--- src/main/java/day2/gugudan/Application.java
+++ src/main/java/day2/gugudan/Application.java
@@ -6,7 +6,18 @@ public class Application {
         NumberUtils numberUtils = new NumberUtils();
         Thread thread = new Thread(numberUtils);
         Thread thread2 = new Thread(numberUtils);
+
         thread.start();
         thread2.start();
+
+        sleepTest();
+    }
+
+    private static void sleepTest() {
+        try {
+            Thread.sleep(3 * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
