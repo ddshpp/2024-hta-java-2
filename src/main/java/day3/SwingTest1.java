6197b96a287f115af8e02efe5746b2c36b286289
ddshpp <dshp753@gmail.com>
2024-05-28 12:39:40 +0900

    style: reformat code

--- src/main/java/day3/SwingTest1.java
+++ src/main/java/day3/SwingTest1.java
@@ -1,6 +1,6 @@
 package day3;
 
-import java.awt.Color;
+import java.awt.event.ComponentAdapter;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -9,27 +9,27 @@ import javax.swing.JPanel;
 public class SwingTest1 extends JFrame {
 
     private JPanel jPanel1 = new JPanel();
-    private JButton button1 = new JButton("Button1");
-    private JButton button2 = new JButton("Button2");
-    private JButton button3 = new JButton("Button3");
+    private JButton button1 = new JButton();
+    private JButton button2 = new JButton();
     private JLabel Label1;
-    private JLabel Label2;
 
     public SwingTest1() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("SwingTest");
         setBounds(100, 100, 500, 500);
         setLocationRelativeTo(null);
-        jPanel1.setBackground(Color.BLUE);
+//        jPanel1.setBackground(Color.BLUE);
         jPanel1.setLayout(null);
 
         button1.setBounds(10, 10, 100, 30);
+        button1.setText("Button1");
         jPanel1.add(button1);
         button2.setBounds(10, 50, 100, 30);
+        button2.setText("Button2");
         jPanel1.add(button2);
-        button3.setBounds(10, 90, 100, 30);
-        jPanel1.add(button3);
 
         setContentPane(jPanel1);
+        jPanel1.addComponentListener(new ComponentAdapter() {
+        });
     }
 }
