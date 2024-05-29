473b5773239bab4c5ae23543652ad8010f3fc227
ddshpp <dshp753@gmail.com>
2024-05-29 09:33:57 +0900

    create: day4

--- src/main/java/day3/Application.java
+++ src/main/java/day3/Application.java
@@ -4,8 +4,6 @@ import javax.swing.JFrame;
 
 public class Application extends JFrame {
     public static void main(String[] args) {
-        System.out.println("day3");
-
         SwingTest1 swingTest1 = new SwingTest1();
         swingTest1.setVisible(true);
     }
